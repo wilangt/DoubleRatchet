c584cd7fa1f9d16dafaa644368701373ae808981
Milan Gonzalez-Thauvin <milan.gonzalez-thauvin@ens-lyon.fr>
2021-01-20 01:55:32 +0100

    encrypt small text

--- rsa.ml
+++ rsa.ml
@@ -108,10 +108,11 @@ let rec check_random_prime lambda = function
 
 (* Cryptosystème RSA *)
 
-type plaintext = Z.t;;
-type ciphertext = Z.t;;
 type public_key = Z.t * Z.t;;
 type secret_key = Z.t * Z.t;;
+type plaintext = Z.t;;
+type ciphertext = Z.t;;
+type ciphertext_message = Z.t;;
 
 let extended_euclidian_algorithm phi e =
 	let rec aux r u v r' u' v' =
@@ -120,23 +121,72 @@ let extended_euclidian_algorithm phi e =
 	in aux phi z1 z0 e z0 z1;;
 
 let find_e_d phi =
+	(* Trouve les valeurs de e et de phi nécéssaires pour générer une paire de clef RSA *)
 	let rec aux e = 
 		let (r, u, v) = extended_euclidian_algorithm phi e in
 		if r = z1 then (e, (Z.erem v phi)) else aux (Z.succ e)
 	in aux z3;;
 
 let generate_keys (lambda : int) : public_key * secret_key =
+	(* Génère une paire de clefs RSA pour un lambda donné *)
 	let p = random_prime (lambda - 1) and q = random_prime (lambda - 1) in
 	let n = p ** q and phi = (Z.pred p) ** (Z.pred q) in
 	let e,d = find_e_d phi in
 	((n, e), (n,d));;
 
 let encrypt (pk : public_key) (m : plaintext) : ciphertext =
+	(* Chiffre un entier *)
 	let n,e = pk in
 	if m < n
 	then Z.powm m e n
-	else failwith "m is out of plaintext space"
+	else failwith "m is out of plaintext space";;
 
 let decrypt (sk : secret_key) (c : ciphertext) : plaintext =
+	(* Déchiffre un entier chiffré *)
 	let n, d = sk in
-	Z.powm c d n
+	Z.powm c d n;;
+
+(*
+let text_to_block (size : int) (s : string) : string list = 
+	(* Coupe une chaine s en chaines de taille au plus size *)
+	let n = String.length s in
+	let rec aux acc = function
+		|i when i < 0 -> (String.sub s 0 (size + i)) :: acc
+		|i -> aux ((String.sub s i size) :: acc) (i - size)
+	in aux [] (n-size);;
+*)
+
+let rec int_to_base b = function
+	(* Écriture en base b de n *)
+	|n when n < b -> [n]
+	|n -> Z.rem n b :: int_to_base b (n//b);;
+
+let rec base_to_int b = function
+	|[] -> z0
+	|t::q -> t ++ b ** (base_to_int b q);;
+
+let string_to_int s =
+	let explode = List.init (String.length s) (String.get s) in
+	let liste_ascii = List.map Char.code explode in
+	let liste_ascii_Z = List.map Z.of_int liste_ascii in
+	base_to_int (Z.of_int 128) liste_ascii_Z;;
+
+let int_to_string n =
+	let liste_ascii_Z = int_to_base (Z.of_int 128) n in
+	let liste_ascii = List.map Z.to_int liste_ascii_Z in
+	let explode = List.map Char.chr liste_ascii in
+	String.concat "" (List.map (String.make 1) explode);;
+
+(*
+let string_to_block s lambda =
+	let lambdaa = lambda - 7 (* nombre de bit d'un caractère ASCII *) - 5 (* marge *) in
+	let block_list = text_to_block lambdaa s in
+	List.map block_list
+*)
+
+let encrypt_message (pk : public_key) (m : string) : ciphertext_message =
+	(* Chiffre un message ASCII *)
+	encrypt pk (string_to_int m);;
+
+let decrypt_message (sk : secret_key) (c : ciphertext_message) : string =
+	int_to_string (decrypt sk c);;
